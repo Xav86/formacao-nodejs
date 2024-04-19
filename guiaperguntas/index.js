cb7340974aef5913a599f642916b98c573b0a8e8
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-04-19 09:34:42 -0300

    instalado bootstrap

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -3,17 +3,30 @@ const app = express();
 
 // Setando Express para usar o EJS como View engine
 app.set("view engine","ejs");
+app.use(express.static('public'));
 
 
 app.get("/:nome/:lang",(req,res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Chocolate", preco: 5.5},
+        {nome: "Guarana", preco: 6},
+        {nome: "Leite-condensado", preco: 5},
+        {nome: "Carne", preco: 15},
+        {nome: "Nescau", preco: 7},
+        {nome: "Cerveja", preco: 5.5}
+    ]
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "Fontana",
         inscritos: 1246,
-    }); //pega automatico o arquivo index na pasta Views, não precisando botar o vaminho "view/index". nem a extrensão precisa.
+        msg: exibirMsg,
+        produtos: produtos,
+    }); //pega automatico o arquivo index na pasta Views, não precisando botar o vaminho "view/index". nem a extrensão .ejs precisa.
 });
 
 app.listen(8080,() => {
