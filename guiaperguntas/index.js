6d31b81bf2f46bf93fb8cff74754690829b312e9
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-04-19 11:54:06 -0300

    recebendo dados do formulario

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Setando Express para usar o EJS como View engine
 app.set("view engine","ejs");
 app.use(express.static('public'));
+
+//config bootstrap5 pra ser estatico
 // app.use(express.static("node_modules/bootstrap/dist/"));
 app.use('/css', express.static('./node_modules/bootstrap/dist/css'));
 app.use('/js', express.static('./node_modules/bootstrap/dist/js'));
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//rotas
 app.get("/",(req,res) => {
     res.render("index");//pega automatico o arquivo index na pasta Views, não precisando botar o vaminho "view/index". nem a extrensão .ejs precisa.
-
 });
 
 app.get("/perguntar",(req,res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req,res) =>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo: " + titulo + " descrição: " + descricao);
 });
 
 app.listen(8080,() => {
