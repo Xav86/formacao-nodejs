2952859f65e26c4300201b24042f71ee4af1e230
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-05-22 11:45:04 -0300

    vendo métodos estaticos

---
+++ POO/Calculadora.js
@@ -0,0 +1,15 @@
+class Calculadora {
+
+    static Somar(a, b){
+        console.log(a+b);
+
+    }
+
+    static Sub(a, b){
+        console.log(a-b);
+    }
+
+}
+
+Calculadora.Somar(2, 3);
+Calculadora.Sub(3, 2);
