f3bc8a3a33de52fb2f73dd13ef903df0b02b89c8
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-05-27 16:31:21 -0300

    feito mini formulario para teste de validação

--- validacao/src/index.js
+++ validacao/src/index.js
@@ -3,6 +3,7 @@ const app = express();
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var flash = require("express-flash");
+var cookieParser = require("cookie-parser");
 
 app.set('view engine','ejs');
 
@@ -12,11 +13,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(cookieParser("-bs%#etghbse34n_iu¨$675re"))
+
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { maxAge: 60000 }
 }))
 
 app.use(flash());
@@ -24,7 +27,22 @@ app.use(flash());
 // ------------------
 
 app.get("/",(req, res) => {
-    res.render("index")
+
+    var emailError = req.flash("emailError");
+    var idadeError = req.flash("idadeError");
+    var nomeError = req.flash("nomeError");
+    var email = req.flash("email");
+    var nome = req.flash("nome");
+    var idade = req.flash("idade");
+
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError;
+    idadeError = (idadeError == undefined || idadeError.length == 0) ? undefined : idadeError;
+    nomeError = (nomeError == undefined || nomeError.length == 0) ? undefined : nomeError;
+    email = (email == undefined || email.length == 0) ? "" : email;
+    nome = (nome == undefined || nome.length == 0) ? "" : nome;
+    idade = (idade == undefined || idade.length == 0) ? "" : idade;
+
+    res.render("index",{emailError,idadeError,nomeError, email: email, nome: nome, idade: idade})
 });
 
 // ---------------- Formulario ----------------
@@ -53,6 +71,14 @@ app.post("/form", (req, res) => {
     }
 
     if(emailError != undefined || idadeError != undefined || nomeError != undefined) {
+        req.flash("emailError",emailError);
+        req.flash("idadeError",idadeError);
+        req.flash("nomeError",nomeError);
+
+        req.flash("email", email);
+        req.flash("nome", nome);
+        req.flash("idade", idade);
+
         res.redirect("/");
     } else {
         res.send("Formulario show da bola");
@@ -61,5 +87,5 @@ app.post("/form", (req, res) => {
 });
 
 app.listen(7324, (req, res) => {
-    console.log("Servidor iniciado")
+    console.log("Servidor iniciado");
 });
