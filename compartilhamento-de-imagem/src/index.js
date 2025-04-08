e0c0bbf460330f10771adf03769919fd4402461f
Xav86 <gustavo.g.2005@gmail.com>
2025-04-08 10:39:51 -0300

    feito a rota user fazendo o teste passar

--- compartilhamento-de-imagem/src/index.js
+++ compartilhamento-de-imagem/src/index.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
+const user = require('./models/user.js');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/guiapics').then(() => {}).catch(error => { console.error(error) });
 
+const User = mongoose.model('User', user);
+
 app.get('/', ( req, res ) => {
     res.status(200).json({});
+});
+
+app.post('/user', async ( req, res ) => {
+    const { name, email, password } = req.body;
+    
+    try {
+        const newUser = new User({ name, email, password })
+        await newUser.save();
+
+        res.json({ email });
+    } catch(error) {
+        console.error(error);
+        res.sendStatus(500)
+    }
 });
 
 module.exports = app;
